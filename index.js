80066c3c8a6001446fe89a6330215d7a0d5a2d39
yunisaliosman <yunis.osman@student.moringaschool.com>
2022-12-06 21:07:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(Jina, events) {
+    const arrayFun = [];
+  
+    for (let n = 0; n < Jina.length; n++) {
+      const info =
+        `Thank you, ${Jina[n]}, for the wonderful ${events} gift!`;
+      arrayFun.push(info);
+    }return arrayFun;
+  }
+  
+  function countDown(x) {
+    do {
+      console.log(x);
+      x--;
+    } 
+    while (x >= 0);
+  }
